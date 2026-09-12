b3585f7738f8c6e56720d10e6f063f3fc5b6b11d
Tanaw <tanaw@local>
2026-09-12 08:18:00 +0700

    fix: update controller map parent offset

--- GameOffsets/Objects/States/InGameState/ImportantUiElementsOffsets.cs
+++ GameOffsets/Objects/States/InGameState/ImportantUiElementsOffsets.cs
@@ -15,7 +15,7 @@ namespace GameOffsets.Objects.States.InGameState
         [FieldOffset(0x730)] public IntPtr PassiveSkillTreePanel;
         [FieldOffset(0x7C0)] public IntPtr MapParentPtr;
         [FieldOffset(0x988)] public IntPtr WorldMapPanelPtr;
-        [FieldOffset(0xB98)] public IntPtr ControllerModeMapParentPtr;
+        [FieldOffset(0xB88)] public IntPtr ControllerModeMapParentPtr;
     }
 
     [StructLayout(LayoutKind.Explicit, Pack = 1)]
